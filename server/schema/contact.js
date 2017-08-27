3dce29e476dc83547f96b51b4c7821b20e74e88e
tolley <chris.tolley@gmail.com>
2017-08-27 19:33:18 -0400

    Cleaned up the graphql code a bit to account for user id, contact id

--- server/schema/contact.js
+++ server/schema/contact.js
@@ -2,48 +2,84 @@
 var gql = require( 'graphql' );
 
 module.exports = {
-	id: {
-		type: gql.GraphQLInt,
-		description: 'The primary key of the contact.'
+	all: {
+		id: {
+			type: gql.GraphQLString,
+			description: 'The primary key identifying a contact'
+		},
+		first_name: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s first name'
+		},
+		last_name: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s last name'
+		},
+		email_address: {
+			type: gql.GraphQLString,
+			description: '' 
+		},
+		phone: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s phone number'
+		},
+		street_address: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s street address'
+		},
+		street_address2: {
+			type: gql.GraphQLString,
+			description: 'The second line of the contact\'s street address'
+		},
+		city: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s city'
+		},
+		state: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s state'
+		},
+		zip: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s zip code'
+		}
 	},
-	user_id: {
-		type: gql.GraphQLInt,
-		description: 'The id of the user that owns this contact'
-	},
-	first_name: {
-		type: gql.GraphQLString,
-		description: 'The contact\'s first name'
-	},
-	last_name: {
-		type: gql.GraphQLString,
-		description: 'The contact\'s last name'
-	},
-	email_address: {
-		type: gql.GraphQLString,
-		description: '' 
-	},
-	phone: {
-		type: gql.GraphQLString,
-		description: 'The contact\'s phone number'
-	},
-	street_address: {
-		type: gql.GraphQLString,
-		description: 'The contact\'s street address'
-	},
-	street_address2: {
-		type: gql.GraphQLString,
-		description: 'The second line of the contact\'s street address'
-	},
-	city: {
-		type: gql.GraphQLString,
-		description: 'The contact\'s city'
-	},
-	state: {
-		type: gql.GraphQLString,
-		description: 'The contact\'s state'
-	},
-	zip: {
-		type: gql.GraphQLString,
-		description: 'The contact\'s zip code'
+	insertable: {
+		first_name: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s first name'
+		},
+		last_name: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s last name'
+		},
+		email_address: {
+			type: gql.GraphQLString,
+			description: '' 
+		},
+		phone: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s phone number'
+		},
+		street_address: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s street address'
+		},
+		street_address2: {
+			type: gql.GraphQLString,
+			description: 'The second line of the contact\'s street address'
+		},
+		city: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s city'
+		},
+		state: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s state'
+		},
+		zip: {
+			type: gql.GraphQLString,
+			description: 'The contact\'s zip code'
+		}
 	}
 }
